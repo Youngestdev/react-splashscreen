efb636de0327752fbe19467c72b2385489d7ac7c
Abdulazeez Abdulazeez Adeshina <31009679+Youngestdev@users.noreply.github.com>
2018-11-08 18:29:57 +0100

    Update CallBack.js

--- src/components/CallBack.js
+++ src/components/CallBack.js
@@ -4,7 +4,7 @@ import auth0Client from '../Auth'
 import Axios from 'axios';
 import firebaseClient from '../services/FireBase';
 
-const tokenFactory = 'https://wt-21d474de14a60e389a0fa7c8e77c224f-0.sandbox.auth0-extend.com/webtasks'
+const tokenFactory = 'https://wt-21d474de14a60e389a0fa7c8e77c224f-0.sandbox.auth0-extend.com/webtasks/firebase'
 
 class Callback extends Component {
   async componentDidMount() {
